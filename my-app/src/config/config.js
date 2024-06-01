1fa3b26e3ddec8c07fec65293007c872bd22d6cd
arun-upadhayay <arunupadhayay10102000@gmail.com>
2024-06-01 14:24:31 +0530

    change in backend url

--- my-app/src/config/config.js
+++ my-app/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: 'http://localhost:4000', // Backend server URL
+  apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:4000/api/v1',
   };
   
   export default config;
